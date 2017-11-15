3b6bb4c72cfc381b0d09e4f3650508cff1c7f095
omkarjadhav21 <omkarjadhav7110@gmail.com>
2017-11-15 18:02:18 +0530

    multi-step form

--- src/app/propertyadd/propertyadd.component.ts
+++ src/app/propertyadd/propertyadd.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HostBinding } from '@angular/core/src/metadata/directives';
 
 @Component({
   selector: 'app-propertyadd',
@@ -11,5 +12,52 @@ export class PropertyaddComponent implements OnInit {
 
   ngOnInit() {
   }
+prev1=this.fun1();
+  tab1:boolean;
+  tab2:boolean
+  tab3:boolean
+  tab4:boolean
+  tab5:boolean
+
+fun1(){
+  this.tab1=true;
+  this.tab2=false
+  this.tab3=false
+  this.tab4=false
+  this.tab5=false
+  console.log("in fun1111");
+}
+fun2(){
+  this.tab1=false;
+  this.tab2=true
+  this.tab3=false
+  this.tab4=false
+  this.tab5=false
+  console.log("in fun1111");
+}
+fun3(){
+  this.tab1=false;
+  this.tab2=false
+  this.tab3=true
+  this.tab4=false
+  this.tab5=false
+  console.log("in fun1111");
+}
+fun4(){
+  this.tab1=false;
+  this.tab2=false
+  this.tab3=false
+  this.tab4=true
+  this.tab5=false
+  console.log("in fun1111");
+}
+fun5(){
+  this.tab1=false;
+  this.tab2=false
+  this.tab3=false
+  this.tab4=false
+  this.tab5=true
+  console.log("in fun1111");
+}
 
 }
